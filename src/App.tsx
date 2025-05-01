3dde691d8a5e4201a2e5823c8d9a2b0667ba63ef
Eryck Mike <eryckmike@MacBook-Air-de-Eryck.local>
2025-05-01 19:16:52 -0300

    add: create enviromment

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
+import { Router } from "./Router"
+import { defaultTheme } from "./styles/themes/deafault"
+import { GlobalStyles } from "./styles/global"
+
+
+function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+      <GlobalStyles/>
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ src/styles/themes/deafault.ts
@@ -0,0 +1,23 @@
+export const defaultTheme = {
+    //Aqui vem as cores base
+  
+    'base-title':'#272221',
+    'base-subtitle': '#403937',
+    'base-text': '#574f4d',
+    'base-label': '#8d8686',
+    'base-hover': '#d7d5d5',
+    'base-button': '#e6e5e5',
+    'base-input': '#ededed',
+    'base-card': '#f3f2f2',
+    'background': '#fafafa',
+    white: '#FFF',
+  
+    //Aqui vem as cores vibrantes
+  
+    'yellow': '#DBAC2C',
+    'yellow-light': '#F1E9C9',
+    'yellow-dark': '#C47F17',
+    'purple': '#8047F8',
+    'purple-light': '#EBE5F9',
+    'purple-dark': '#4B2995',
+  }
