612a6ef87cefa57a26a5fad7e22794c748bba3d4
carollyra <carolina.magnani.lyra@gmail.com>
2025-05-30 16:14:12 -0300

    .

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,28 +1,29 @@
 import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import { SignLayout } from "./layouts/SignLayout";
+
 import { Home } from "./pages/Home";
-import { Frotas } from "./pages/Frota"
+import { Frotas } from "./pages/Frota";
 import { Motoristas } from "./pages/Motoristas";
 import { Sign } from "./pages/Sign";
 import { SignUp } from "./pages/SignUp";
+import CadastroMotorista from "./pages/SignUpMotoristas"; 
 
 export function Router() {
   return (
     <Routes>
-        <Route path="/" element={<DefaultLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/frotas" element={<Frotas/>} />
-            <Route path="/motoristas" element={<Motoristas/>}/>
-        </Route>
-        <Route path="/" element={<SignLayout/>}>
-            <Route path="/sign" element={<Sign/>}/>
-            <Route path="/signup" element={<SignUp/>}/>
-        </Route>
+      {/* Layout padrão da aplicação */}
+      <Route path="/" element={<DefaultLayout />}>
+        <Route index element={<Home />} />
+        <Route path="frotas" element={<Frotas />} />
+        <Route path="motoristas" element={<Motoristas />} />
+        <Route path="cadastro" element={<CadastroMotorista />} /> 
+      </Route>
+
+      <Route path="/" element={<SignLayout />}>
+        <Route path="sign" element={<Sign />} />
+        <Route path="signup" element={<SignUp />} />
+      </Route>
     </Routes>
-  )
+  );
 }
-
-import CadastroMotorista from "./pages/SignUpMotoristas";
-<Route path="/cadastro" element={<CadastroMotorista />} />
-
